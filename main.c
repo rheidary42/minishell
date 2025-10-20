a23c5755328ca5cb0c3afa82239bd30803df32e4
Radin Heidary <rheidary@Radins-MacBook-Pro.local>
2025-10-20 17:44:53 +0200

    Added skeleton for main and headers, empty file for parsing

---
+++ main.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rheidary <rheidary@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/19 16:53:58 by rheidary          #+#    #+#             */
+/*   Updated: 2025/10/20 15:41:42 by rheidary         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int main(int ac, char **av, char **envp)
+{
+	char	*line;
+
+	(void)ac; (void)av; (void)envp;
+	while (true)
+	{
+		line = readline("minishell>");
+		if (!line)	// If EOF (CTRL-D) detected exit
+			exit (0);
+		if (line[0] != '\0')	// Check for empty input to not clog history with empty lines
+			add_history(line);
+		parse();
+		// execute
+		// free
+	}
+	return 0;
+}
---
+++ minishell.h
@@ -0,0 +1,42 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+/* External libraries*/
+# include "libs/libft/libft.h"
+
+/* Standard library + memory + strings */
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdbool.h>
+
+/* File descriptors, open(), access(), stat(), etc */
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+
+/* Directory handling (opendir, readdir, closedir) */
+# include <dirent.h>
+
+/* Errors (perror, strerror) */
+# include <errno.h>
+
+/* Signals (signal, sigaction, sigemptyset, sigaddset, kill) */
+# include <signal.h>
+
+/* Waiting (wait, waitpid, wait3, wait4) */
+# include <sys/wait.h>
+
+/* TTY / Terminal control (isatty, ttyname, ttyslot, ioctl) */
+# include <sys/ioctl.h>
+
+/* Termcap / Termios (tgetent, tputs, etc) */
+# include <termios.h>
+# include <termcap.h>
+
+/* Readline */
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
