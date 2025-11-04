88421970f4d8042acb7efe9b559b05c94a2a1f5d
Radin Heidary <rheidary@c3r7p1.42vienna.com>
2025-11-04 18:44:37 +0100

    pushed to work at home

--- expansion.c
+++ expansion.c
@@ -6,15 +6,83 @@
 /*   By: rheidary <rheidary@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 20:23:59 by rheidary          #+#    #+#             */
-/*   Updated: 2025/11/02 20:24:57 by rheidary         ###   ########.fr       */
+/*   Updated: 2025/11/04 18:05:39 by rheidary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	match_connection()
+{
+	
+}
+
+int	find_var(t_shell *shell)
+{
+	t_token	*curr;
+
+	curr = shell->tokens;
+	while (shell->tokens != NULL)
+	{
+		if (match_connection(curr->value))
+			
+	}
+}
+
+void	do_expansion(t_shell *shell)
+{
+	int	i;
+
+	i = 0;
+	while (shell->tokens->value[i] != '\0')
+	{
+		if (shell->tokens->value[i] == '$')
+		{
+			find_var(shell);
+			i = 0;
+		}
+		i++;
+	}
+}
+
+int	needs_expansions(t_token *token)
+{
+	int	i;
+
+	if (token->type != TOKEN_WORD || token->value == NULL)
+		return (0);
+	i = 0;
+	while (token->value[i])
+	{
+		if (token->value[i] == '\'')
+		{
+			i++;
+			while (token->value[i] != '\'')
+				i++;
+		}
+		if (token->value[i] == '$' && ft_isalpha(token->value[i + 1]
+				|| token->value[i + 1] == '?'))
+			return (1);
+	}
+	return (0);
+}
+
+void	parameter(t_shell *shell)
+{
+	t_token	*curr;
+
+	curr = shell->tokens;
+	while (curr != NULL)
+	{
+		if (needs_expansions(shell->tokens))
+			do_expansion(shell);
+		curr = curr->next;
+	}
+}
+
 void	expand(t_shell *shell)
 {
-	parameter();
+	parameter(shell);
 	word();
 	quote();
 }
--- parse.c
+++ parse.c
@@ -6,7 +6,7 @@
 /*   By: rheidary <rheidary@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 12:23:09 by rheidary          #+#    #+#             */
-/*   Updated: 2025/11/02 20:06:48 by rheidary         ###   ########.fr       */
+/*   Updated: 2025/11/04 18:10:02 by rheidary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,18 @@ t_token	*tokenize(t_shell *shell)
 	arr = split(shell);
 	build_token_list(shell, arr);
 	// clean arguments
-	// if (!validate_tokens((*shell)->tokens))
-	// {
-	// 	//cleanup
-	// 	exit(1);
-	// }
-	build_commands(shell);
-	//assign_values();
+	if (!validate_tokens(shell->tokens))
+	{
+		//cleanup
+		exit(1);
+	}
 	return (NULL);
 }
 
 void	parse(t_shell *shell)
 {
 	tokenize(shell);
+	build_commands(shell);
 	// while (state == in_quote)
 	// {
 	// 	readline()
