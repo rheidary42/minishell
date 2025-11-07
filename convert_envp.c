5ec67a66ad5b948b75da9b2f7881f803614f9302
Borkhuu Enkhjargal <boenkhja@c3r6p7.42vienna.com>
2025-11-07 05:10:01 +0100

    simple converting from linked list to char**

---
+++ convert_envp.c
@@ -0,0 +1,35 @@
+#include "minishell.h"
+
+int	env_list_size(t_env *env)
+{
+	int	i;
+
+	i = 0;
+	while (env != NULL)
+	{
+		i++;
+		env = env->next;
+	}
+	return (i);
+}
+
+char	**convert_envp(t_shell *shell)
+{
+	t_env	*curr_env;
+	char	**env;
+	int		list_size;
+	int		i;
+
+	curr_env = shell->env;
+	list_size = env_list_size(curr_env);
+	env = safe_calloc(sizeof(char *) * (list_size + 1), shell);
+	i = 0;
+	while (curr_env != NULL)
+	{
+		env[i] = str_join3(curr_env->name, "=", curr_env->value, shell);
+		i++;
+		curr_env = curr_env->next;
+	}
+	env[i] = NULL;
+	return (env);
+}
