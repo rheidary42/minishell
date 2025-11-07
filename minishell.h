9eb54e42493c04313d0765751eb004a0b574264e
Borkhuu Enkhjargal <boenkhja@c3r6p7.42vienna.com>
2025-11-07 05:15:14 +0100

    added some function prototypes

--- minishell.h
+++ minishell.h
@@ -122,8 +122,8 @@ void	clean_up(t_shell *shell);
 char	*ft_safe_strdup(const char *s, t_shell *shell);
 char	*ft_strcpy(char *dest, char *src);
 
-        //EXECUTION HELPEERS
-char	*custom_strjoin(char const *s1, char const *s2, t_shell *shell);
+        //EXECUTION HELPERS
+char	*str_join3(char *s1, char *s2, char *s3, t_shell *shell);
 bool	is_direct_path(char *executable);
 void	initialise_exec(t_exec *exec);
 int     count_path(char *paths_from_env);
@@ -137,5 +137,9 @@ void	free_and_close(t_shell *shell, t_cmd *cmd, t_exec *exec);
 int	exec_ext_cmd(t_cmd *cmd, t_shell *shell, t_exec *exec);
 int	exec_single_cmd(t_shell *shell, t_exec *exec);
 int	exec_in_child(t_shell *shell, t_cmd *cmd, t_exec *exec);
+
+int	make_envp(t_env *copy_env, char **curr_env);
+char	**convert_envp(t_shell *shell);
+int	free_env_list(t_env *head);
 
 #endif
