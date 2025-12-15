fd8a385a3269f9aaf725ec954f7e445162484eaf
Borkhuu Enkhjargal <boenkhja@c1r5p5.42vienna.com>
2025-12-15 13:12:53 +0100

    added null check for convert_envp and removed apply_redir from exec_in_child

--- exec_single_command.c
+++ exec_single_command.c
@@ -34,20 +34,30 @@ void	exec_in_child(t_shell *shell, t_cmd *cmd, t_exec *exec)
 {
 	char	**envp;
 
-	apply_redir(shell, cmd, cmd->redir, exec);
+	if ((envp = convert_envp(shell)) == NULL)
+	{
+		free_and_close(shell, exec, envp);
+		exit(1);
+	}
+	if (is_builtin(cmd->argv[0]) == 1)
+	{
+		shell->last_exit_status = exec_builtin(cmd);
+		free_and_close(shell, exec, envp);
+		exit(shell->last_exit_status);
+	}
 	if (is_direct_path(cmd->argv[0]) == true)
-		exec->final_path = cmd->argv[0];
+		exec->final_path = ft_strdup(cmd->argv[0]);
 	else
 		path_lookup(cmd->argv[0], shell, exec);
 	if (exec->final_path == NULL)
 	{
 		printf("%s: command not found\n", cmd->argv[0]);
-		free_and_close(shell, cmd, exec);
+		free_and_close(shell, exec, envp);
 		exit(127);
 	}
-	envp = convert_envp(shell);
 	execve(exec->final_path, cmd->argv, envp);
 	perror(cmd->argv[0]);
+	free_and_close(shell, exec, envp);
 	exit(127);
 }
 
