ea899df8ec0cb2fb5e5c3d44af08f607dc08c2ad
Borkhuu Enkhjargal <boenkhja@c1r5p5.42vienna.com>
2025-12-15 13:13:39 +0100

    added is_builtin function

--- execution.c
+++ execution.c
@@ -1,5 +1,24 @@
 #include "minishell.h"
 
+int	is_builtin(const char *cmd)
+{
+	if (ft_strcmp(cmd, "echo") == 0)
+		return (1);
+	if (ft_strcmp(cmd, "cd") == 0)
+		return (1);
+	if (ft_strcmp(cmd, "pwd") == 0)
+		return (1);
+	if (ft_strcmp(cmd, "export") == 0)
+		return (1);
+	if (ft_strcmp(cmd, "unset") == 0)
+		return (1);
+	if (ft_strcmp(cmd, "env") == 0)
+		return (1);
+	if (ft_strcmp(cmd, "exit") == 0)
+		return (1);
+	return (0);
+}
+
 int	execution(t_shell *shell)
 {
 	t_exec	exec;
