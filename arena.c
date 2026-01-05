09ed6fc2d76fa11ccf37666f798e3056a5c3e4d7
Radin Heidary <radinheidary2012@gmail.com>
2026-01-05 21:04:43 +0100

    feat: Add Arena Allocator
    - Added Arena Allocator to project. Potentionally refactor to use arena
      for Memory Management after deliberation with partner.

---
+++ arena.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   arena.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rheidary <rheidary@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/05 16:08:31 by rheidary          #+#    #+#             */
+/*   Updated: 2026/01/05 16:08:37 by rheidary         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_mem_arena	*arena_create(t_u64 capacity)
+{
+	t_mem_arena	*arena;
+
+	arena = (t_mem_arena *)malloc(capacity);
+	if (arena == NULL)
+	{
+		return (NULL);
+	}
+	arena->capacity = capacity;
+	arena->pos = sizeof(t_mem_arena);
+	return (arena);
+}
+
+
+void	*arena_push(t_mem_arena *arena, t_u64 size, t_b32 non_zero)
+{
+	t_u64	pos_aligned;
+	t_u64	new_pos;
+	t_u8	*out;
+
+	pos_aligned = (((t_u64)(arena->pos) + ((t_u64)(sizeof(void *)) - 1))
+			& (~((t_u64)(sizeof(void *)) - 1)));
+	new_pos = pos_aligned + size;
+	if (new_pos > arena->capacity)
+	{
+		return (NULL);
+	}
+	arena->pos = new_pos;
+	out = (t_u8 *)arena + pos_aligned;
+	if (!non_zero)
+	{
+		ft_memset(out, 0, size);
+	}
+	return (out);
+}
+
+void	arena_pop(t_mem_arena *arena, t_u64 size)
+{
+	t_u64	max_pop;
+
+	max_pop = arena->pos - sizeof(t_mem_arena);
+	if (size > max_pop)
+		size = max_pop;
+	arena->pos -= size;
+}
+
+void	arena_pop_to(t_mem_arena *arena, t_u64 pos)
+{
+	t_u64	size;
+
+	size = 0;
+	if (pos < arena->pos)
+		size = arena->pos - pos;
+	arena_pop(arena, size);
+}
+
+void	arena_clear(t_mem_arena *arena)
+{
+	arena_pop_to(arena, sizeof(void *));
+}
+
